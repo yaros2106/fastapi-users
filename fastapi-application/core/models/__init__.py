2c75c14dbcf0d7218ed04acdd7bfe8cd80286d42
Yaroslav <iaroslav.bierioza@gmail.com>
2025-11-04 13:33:29 +0300

    create User model

--- fastapi-application/core/models/__init__.py
+++ fastapi-application/core/models/__init__.py
@@ -1,10 +1,8 @@
 __all__ = (
     "db_helper",
     "Base",
-    "UserModel",
 )
 
 
 from .db_helper import db_helper
 from .base import Base
-from .user import UserModel
--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,13 +1,11 @@
-from sqlalchemy import UniqueConstraint
-from sqlalchemy.orm import Mapped, mapped_column
-
-from .base import Base
-from .mixins.int_id_pk import IntIdPkMixin
+from fastapi_users.db import (
+    SQLAlchemyBaseUserTable,
+)
 
 
-class UserModel(IntIdPkMixin, Base):
-    username: Mapped[str] = mapped_column(unique=True)
-    foo: Mapped[int]
-    bar: Mapped[int]
+from core.models import Base
+from core.models.mixins.int_id_pk import IntIdPkMixin
 
-    __table_args__ = (UniqueConstraint("foo", "bar"),)
+
+class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
+    pass
