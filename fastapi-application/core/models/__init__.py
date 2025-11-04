6a5c1503fff474e0c23a638da40331fa6cf00a05
Yaroslav <iaroslav.bierioza@gmail.com>
2025-11-04 14:50:03 +0300

    create access_token migration

--- fastapi-application/core/models/__init__.py
+++ fastapi-application/core/models/__init__.py
@@ -2,9 +2,11 @@ __all__ = (
     "db_helper",
     "Base",
     "User",
+    "AccessToken",
 )
 
 
 from .db_helper import db_helper
 from .base import Base
 from .user import User
+from .access_token import AccessToken
