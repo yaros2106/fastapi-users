294321f3d0d021b0adaf1f2eb066ee3befeb5101
Yaroslav <iaroslav.bierioza@gmail.com>
2025-11-05 13:46:40 +0300

    create get database strategy dependency

---
+++ fastapi-application/api/dependencies/strategy.py
@@ -0,0 +1,25 @@
+from typing import TYPE_CHECKING, Annotated
+
+from fastapi import Depends
+from fastapi_users.authentication.strategy.db import (
+    DatabaseStrategy,
+)
+
+from api.dependencies.access_tokens import get_access_tokens_db
+from core.config import settings
+
+if TYPE_CHECKING:
+    from core.models import AccessToken
+    from fastapi_users.authentication.strategy.db import AccessTokenDatabase
+
+
+def get_database_strategy(
+    access_tokens_db: Annotated[
+        "AccessTokenDatabase[AccessToken]",
+        Depends(get_access_tokens_db),
+    ],
+) -> DatabaseStrategy:
+    return DatabaseStrategy(
+        database=access_tokens_db,
+        lifetime_seconds=settings.access_token.lifetime_seconds,
+    )
