3db33298c6a9c5566f0b31edc9517129fbac4761
Yaroslav <iaroslav.bierioza@gmail.com>
2025-11-07 15:26:43 +0300

    optional required verification

--- fastapi-application/api/api_v1/auth.py
+++ fastapi-application/api/api_v1/auth.py
@@ -13,7 +13,11 @@ router = APIRouter(
 # /login
 # /logout
 router.include_router(
-    router=fastapi_users.get_auth_router(authentication_backend),
+    router=fastapi_users.get_auth_router(
+        authentication_backend,
+        # requires_verification=True,
+    ),
+    prefix=settings.api.v1.auth,
 )
 
 
