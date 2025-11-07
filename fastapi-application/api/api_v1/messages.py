7a623e314d0e5434c968f050308025d64c62b861
Yaroslav <iaroslav.bierioza@gmail.com>
2025-11-07 16:07:55 +0300

    create views for authenticated users and superusers

--- fastapi-application/api/api_v1/messages.py
+++ fastapi-application/api/api_v1/messages.py
@@ -1,9 +1,43 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
 
+from api.api_v1.fastapi_users_router import (
+    current_active_user,
+    current_active_superuser,
+)
 from core.config import settings
-
+from core.models import User
+from core.schemas.user import UserRead
 
 router = APIRouter(
     prefix=settings.api.v1.messages,
     tags=["Messages"],
 )
+
+
+class MessageResponse(BaseModel):
+    messages: list[str]
+    user: UserRead
+
+
+@router.get("", response_model=MessageResponse)
+def get_user_messages(
+    user: User = Depends(current_active_user),
+):
+    return MessageResponse(
+        messages=["m1", "m2", "m3", "m4"],
+        user=UserRead.model_validate(user),
+    )
+
+
+@router.get(
+    "/secrets",
+    response_model=MessageResponse,
+)
+def get_superuser_messages(
+    user: User = Depends(current_active_superuser),
+):
+    return {
+        "messages": ["secret-m1", "secret-m2", "secret-m3", "secret-m4"],
+        "user": UserRead.model_validate(user),
+    }
